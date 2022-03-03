2f2ff4ec7a6f098a3143468db91ae65923ba5161
“codehaoran” <3144975484@qq.com>
2022-03-03 08:41:26 +0800

    左侧分类完成

---
+++ src/api/home.js
@@ -0,0 +1,11 @@
+// 首页api
+import request from '@/utils/request'
+
+/**
+ * 获取品牌
+ * @param {Integer} limit - 品牌个数
+ * @returns {*|AxiosPromise}
+ */
+export const findBrand = (limit = 6) => {
+  return request('/home/brand', 'get', { limit })
+}
